31c5b7f4ff0b9997a4314fe4e45e414edf6cb112
Salo Avalos <saloavalosc@gmail.com>
2022-08-12 15:21:55 -0500

    Profile page is responsive

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -23,59 +23,67 @@ export default () => {
       {!profileData && <div>Loading...</div>}
       {profileData && (
         <div className="entrepreneur-profile-c">
-          <div className="profile__banner-and-profile-photo-c">
-            <div className="profile__top-banner-c">
-              <img
-                src={`https://picsum.photos/id/${randomIDBanner}/600/150`}
-                alt=""
-              />
-            </div>
-            <div className="profile__user-photo-c">
-              <img
-                src={`https://picsum.photos/id/${randomIDProfile}/200/200`}
-                alt="Profile photo"
-              />
-            </div>
+          <div className="profile__banner-c">
+            <img
+              src={`https://picsum.photos/id/${randomIDBanner}/1000/400`}
+              alt=""
+            />
+          </div>
+          <div className="profile__user-photo-c">
+            <img
+              src={`https://picsum.photos/id/${randomIDProfile}/200/200`}
+              alt="Profile photo"
+            />
           </div>
 
-          <div className="profile__all-text-c">
-            <div className="profile__username-c">
-              <p>{profileData.name}</p>
-              <span>@{profileData.username}</span>
-            </div>
-            <div className="profile__main-actions-btns-c">
-              <CustomButton type="primary">View portfolio</CustomButton>
-              <CustomButton color="btn-black">+ Follow</CustomButton>
-            </div>
-            <div className="profile__company-c">
-              <h6 className="profile-subtitle">Company</h6>
-              <p>{profileData.company.name}</p>
-              <span className="profile-desc">
-                {profileData.company.catchPhrase}
-              </span>
-            </div>
-            <hr />
-            <div className="profile__about-me-c">
-              <h6 className="profile-subtitle">About me</h6>
-              <p className="profile-desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi distinctio porro sit in consectetur alias velit unde
-                ipsa ab labore
-              </p>
-              <CustomButton type="secondary">Read more</CustomButton>
-            </div>
-            <div className="profile__extra-info-c">
-              <div className="extra-info__each-c">
-                <p className="profile__extra-info_title">Location</p>
-                <span className="profile-desc">{profileData.address.city}</span>
+          <div className="profile__all-info-c">
+            <div className="profile__main-info">
+              <div className="profile__username-c">
+                <p>{profileData.name}</p>
+                <span>@{profileData.username}</span>
               </div>
-              <div className="extra-info__each-c">
-                <p className="profile__extra-info_title">Website</p>
-                <span className="profile-desc">{profileData.website}</span>
+              <div className="profile__main-actions-btns-c">
+                <CustomButton type="primary">View portfolio</CustomButton>
+                <CustomButton color="btn-black">+ Follow</CustomButton>
               </div>
-              <div className="extra-info__each-c">
-                <p className="profile__extra-info_title">Email</p>
-                <span className="profile-desc">{profileData.email}</span>
+            </div>
+
+            <div className="profile__more-info-c">
+              <div className="profile__sub-info">
+                <div className="profile__company-c">
+                  <h6 className="profile-subtitle">Company</h6>
+                  <p>{profileData.company.name}</p>
+                  <span className="profile-desc">
+                    {profileData.company.catchPhrase}
+                  </span>
+                </div>
+                <hr className="divisor-line" />
+                <div className="profile__about-me-c">
+                  <h6 className="profile-subtitle">About me</h6>
+                  <p className="profile-desc">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eligendi distinctio porro sit in consectetur alias velit
+                    unde ipsa ab labore
+                  </p>
+                  <CustomButton type="secondary">Read more</CustomButton>
+                </div>
+              </div>
+
+              <div className="profile__extra-info-c">
+                <div className="extra-info__each-c">
+                  <p className="profile__extra-info_title">Location</p>
+                  <span className="profile-desc">
+                    {profileData.address.city}
+                  </span>
+                </div>
+                <div className="extra-info__each-c">
+                  <p className="profile__extra-info_title">Website</p>
+                  <span className="profile-desc">{profileData.website}</span>
+                </div>
+                <div className="extra-info__each-c">
+                  <p className="profile__extra-info_title">Email</p>
+                  <span className="profile-desc">{profileData.email}</span>
+                </div>
               </div>
             </div>
           </div>
